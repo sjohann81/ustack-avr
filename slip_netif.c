3504c3e5496b092de914f6c68543964fd2785b61
sergio johann filho <sergio.johann@acad.pucrs.br>
2021-10-23 14:24:03 -0300

    updated SLIP if

--- slip_netif.c
+++ slip_netif.c
@@ -8,44 +8,30 @@
 
 int32_t if_setup()
 {
-	rs232_init(57600);
+	uart_init(57600, 0);
 	
 	return 0;
-}
-
-static void tty_write(uint8_t byte)
-{
-	rs232_tx(byte);
-}
-
-static uint8_t tty_read(void)
-{
-	uint8_t byte;
-	
-	byte = rs232_rx();
-	
-	return byte;
 }
  
 uint16_t netif_send(uint8_t *packet, uint16_t len)
 {
 	uint16_t i;
 
-	tty_write(SLIP_END);
+	uart_tx(SLIP_END);
 	for (i = 0; i < len; i++) {
 		if (packet[i] == SLIP_END) {
-			tty_write(SLIP_ESC);
-			tty_write(SLIP_ESC_END);
+			uart_tx(SLIP_ESC);
+			uart_tx(SLIP_ESC_END);
 		} else {
 			if (packet[i] == SLIP_ESC) {
-				tty_write(SLIP_ESC);
-				tty_write(SLIP_ESC_ESC);
+				uart_tx(SLIP_ESC);
+				uart_tx(SLIP_ESC_ESC);
 			} else {
-				tty_write(packet[i]);
+				uart_tx(packet[i]);
 			}
 		}
 	}
-	tty_write(SLIP_END);
+	uart_tx(SLIP_END);
 	
 	return len;
 }
@@ -56,14 +42,14 @@ uint16_t netif_recv(uint8_t *packet)
 	int16_t r;
 
 	while (1) {
-		r = tty_read();
+		r = uart_rx();
 		if (r == SLIP_END) {
 			if (len > 0) {
 				return len;
 			}
 		} else {
 			if (r == SLIP_ESC) {
-				r = tty_read();
+				r = uart_rx();
 				if (r == SLIP_ESC_END) {
 					r = SLIP_END;
 				} else {
